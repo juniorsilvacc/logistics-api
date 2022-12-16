6f8317624c5f808aa5d119170fa699e5b475ff34
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-12-15 23:15:52 -0300

    Padrão DTO para Cliente

--- src/main/java/com/juniorsilvacc/logistics/controllers/ClienteController.java
+++ src/main/java/com/juniorsilvacc/logistics/controllers/ClienteController.java
@@ -1,6 +1,7 @@
 package com.juniorsilvacc.logistics.controllers;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.juniorsilvacc.logistics.domain.dtos.ClienteDTO;
 import com.juniorsilvacc.logistics.domain.model.Cliente;
 import com.juniorsilvacc.logistics.services.ClienteService;
 
@@ -19,9 +21,10 @@ public class ClienteController {
 	private ClienteService service;
 
 	@GetMapping
-	public ResponseEntity<List<Cliente>> findAll() {
+	public ResponseEntity<List<ClienteDTO>> findAll() {
 		List<Cliente> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<ClienteDTO> listDTO = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
---
+++ src/main/java/com/juniorsilvacc/logistics/domain/dtos/ClienteDTO.java
@@ -0,0 +1,59 @@
+package com.juniorsilvacc.logistics.domain.dtos;
+
+import com.juniorsilvacc.logistics.domain.model.Cliente;
+
+public class ClienteDTO {
+	
+	protected Long id;
+	
+	protected String nome;
+	
+	protected String email;
+	
+	protected String telefone;
+	
+	public ClienteDTO() {
+		super();
+	}
+
+	public ClienteDTO(Cliente obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.email = obj.getEmail();
+		this.telefone = obj.getTelefone();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+}
